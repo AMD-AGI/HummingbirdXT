a0d683c88c81051ec7409b51746087113653db70
hecui <hecui102@amd.com>
2026-01-20 12:18:35 +0800

    update inference script

--- infer/run_i2v.sh
+++ infer/run_i2v.sh
@@ -5,11 +5,11 @@ ROCR_VISIBLE_DEVICES=0 python3 examples/wan2.2/predict_ti2v_single.py \
   --outdir output \
   --seed 123 --shift 10 --st 0 --ed 20 \
   --H 704 --W 1280 --video_length 121 --fps 24 \
-  --i2v_prompt_file image_file \
+  --i2v_prompt_file prompt.txt \
   --infer_steps 3 \
   --cfg 1.0 \
   --sr_model_path ./models/sr/sr_v3.pth \
-  --valid_image_path prompt.txt \
+  --valid_image_path image_file \
   --vae_model ./models/vae/wan22_v1_tiling_16_12 --t_block_size 16 --t_stride 12 
   #--is_sr 
   #--nsfw_detection \
